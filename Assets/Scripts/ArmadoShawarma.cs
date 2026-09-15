d2af975464b2859687ccc35b30c406f123327bf7
SigmaGames-UG <sigmagames763@gmail.com>
2026-09-14 23:10:44 -0300

    ARMADO

--- Assets/Scripts/ArmadoShawarma.cs
+++ Assets/Scripts/ArmadoShawarma.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ArmadoShawarma : MonoBehaviour
 {
-    // Nuestra estructura estática importada de la carpeta "Data Structures"
+    
     private ISimpleStack<GameObject> pilaIngredientes = new SimpleArrayStack<GameObject>(10);
 
     public Transform puntoDeApoyo;
@@ -12,7 +12,7 @@ public class ArmadoShawarma : MonoBehaviour
     public GameObject prefabCarnePlana;
     public GameObject prefabLechugaPlana;
     public GameObject prefabKetchupPlano;
-    // Podés agregar más variables acá a medida que crees nuevos ingredientes
+  
 
     public bool PuedeAgregar()
     {
@@ -25,7 +25,7 @@ public class ArmadoShawarma : MonoBehaviour
 
         GameObject prefabAUsar = null;
 
-        // El switch revisa qué tenés en la mano y elige el asset plano correspondiente
+       
         switch (nombreIngredienteEnMano)
         {
             case "Carne":
@@ -42,7 +42,7 @@ public class ArmadoShawarma : MonoBehaviour
                 return;
         }
 
-        // Si el asset de la ranura está vacío, no hace nada para evitar errores
+     
         if (prefabAUsar == null) return;
 
         Vector3 posicionAlta = puntoDeApoyo.position + new Vector3(0, pilaIngredientes.Count * alturaPorIngrediente, 0);
@@ -50,14 +50,14 @@ public class ArmadoShawarma : MonoBehaviour
         GameObject nuevoIngrediente = Instantiate(prefabAUsar, posicionAlta, Quaternion.identity);
         nuevoIngrediente.transform.SetParent(puntoDeApoyo);
 
-        // ¡Lo metemos en la Pila!
+        
         pilaIngredientes.Push(nuevoIngrediente);
     }
 
-    // El jugador se equivocó: ¡A la basura!
+ 
     public void TirarShawarma()
     {
-        // Mientras la pila NO esté vacía, sacamos el de más arriba y lo destruimos
+        
         while (!pilaIngredientes.IsEmpty())
         {
             GameObject ingredienteArruinado = pilaIngredientes.Pop();
