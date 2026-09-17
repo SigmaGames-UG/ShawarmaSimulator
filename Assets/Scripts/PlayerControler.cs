367e6553ff9ca121585b3d917df6369787a6e87f
SigmaGames-UG <sigmagames763@gmail.com>
2026-09-17 00:28:58 -0300

    .

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -4,19 +4,18 @@ public class Yahya : MonoBehaviour
 {
     [Header("Configuración de Movimiento")]
     public float velocidad = 5f;
-
+    public float gravedad = -9.81f; 
     [Header("Configuración de Cámara")]
     public float sensibilidadMouse = 2f;
-    public Transform camaraJugador; 
+    public Transform camaraJugador;
 
     private CharacterController controller;
     private float rotacionVertical = 0f;
+    private Vector3 velocidadCaida; 
 
     void Start()
     {
-        
         controller = GetComponent<CharacterController>();
-
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -32,27 +31,28 @@ public class Yahya : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        
         Vector3 movimiento = transform.right * x + transform.forward * z;
-
-        
         controller.Move(movimiento * velocidad * Time.deltaTime);
+
+       
+        if (controller.isGrounded && velocidadCaida.y < 0)
+        {
+            velocidadCaida.y = -30f; 
+        }
+
+        velocidadCaida.y += gravedad * Time.deltaTime;
+        controller.Move(velocidadCaida * Time.deltaTime);
     }
 
     void RotarCamara()
     {
-        
         float mouseX = Input.GetAxis("Mouse X") * sensibilidadMouse;
         float mouseY = Input.GetAxis("Mouse Y") * sensibilidadMouse;
 
-        
         rotacionVertical -= mouseY;
         rotacionVertical = Mathf.Clamp(rotacionVertical, -90f, 90f);
 
-        
         camaraJugador.localRotation = Quaternion.Euler(rotacionVertical, 0f, 0f);
-
-       
         transform.Rotate(Vector3.up * mouseX);
     }
 }
